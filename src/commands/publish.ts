9bb16a6c12226ccd7396eb63a913ae85b04e31c4
Ricardo Rocha <me@rrocha.uk>
2022-02-07 17:59:25 +0000

    :fire:

--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -1,31 +1,29 @@
-import {Command} from '@oclif/core'
-import { exec } from 'child_process'
-import * as fs from 'fs';
-import * as archiver from 'archiver'
+import { Command } from "@oclif/core";
+import { exec } from "child_process";
+import * as fs from "fs";
+import * as archiver from "archiver";
 export default class Publish extends Command {
-  static description = 'Publish dist/ folder'
+  static description = "Publish dist/ folder";
 
-  static examples = [
-    `$ rrochacli publish`,
-  ]
+  static examples = [`$ rrochacli publish`];
 
-  static flags = {}
+  static flags = {};
 
-  static args = []
+  static args = [];
 
   async run(): Promise<void> {
     console.log(__dirname);
-    const output = fs.createWriteStream(__dirname + '/res/test.zip');
-    
-    const arch = archiver('zip');
+    const output = fs.createWriteStream(__dirname + "/res/test.zip");
 
-    output.on('close', () => {
+    const arch = archiver("zip");
+
+    output.on("close", () => {
       console.log(arch.pointer());
-      console.log('done...');
+      console.log("done...");
     });
 
     arch.pipe(output);
-    arch.append('hello.txt');
+    arch.append("hello.txt", { name: "hello.txt" });
     arch.finalize();
   }
 }
