cdaa38e8573ef80bc8a975b2ecbf0a44e665182b
Ricardo Rocha <me@rrocha.uk>
2022-02-09 09:10:17 +0000

    * more goodies;

--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -1,8 +1,15 @@
-import { CliUx, Command } from "@oclif/core";
-import * as fs from "fs";
+import { CliUx, Command } from '@oclif/core';
+import * as fs from 'fs';
+import * as fsp from 'fs/promises';
 import * as archiver from "archiver";
+import axios from "axios";
+import * as FormData from 'form-data';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export default class Publish extends Command {
-  static description = "Publish dist/ folder";
+  static description = 'Publish dist/ folder';
 
   static examples = [`$ rrochacli publish`];
 
@@ -13,7 +20,10 @@ export default class Publish extends Command {
   async run(): Promise<void> {
     const fileName = process.cwd().split('/')[process.cwd().split('/').length - 1];
     const dir = await CliUx.ux.prompt(`Publish current directory? "/${fileName}" (yes:no)`);
-    const name = await CliUx.ux.prompt('Enter a name for your app:');
+    const name = await CliUx.ux.prompt('Enter a name for your app');
+    const apiKey = await CliUx.ux.prompt('Enter your api key');
+
+    const form = new FormData();
 
     if (dir === 'yes') {
       const output = fs.createWriteStream(process.cwd() + `/${name}.zip`);
@@ -26,7 +36,26 @@ export default class Publish extends Command {
 
       arch.pipe(output);
       arch.directory(process.cwd(), false);
-      arch.finalize();
+      await arch.finalize();
+
+      // fs.readFile(`${process.cwd()}/${fileName}`, (error, data) => {
+      //   form.append('project', data);
+      // });
+      const data = await fsp.readFile(`${process.cwd()}/${fileName}`);
+      form.append('project', data);
+
+      axios.post('https://cli.ricr.net/', {
+        form,
+      }, {
+        headers: {
+          'api-key': apiKey,
+        }
+      }).then((r) => {
+        console.log(r.data.m);
+      }).catch((e) => {
+        console.log(e);
+      });
+
     } else {
       this.error('Invalid directory!');
     }
